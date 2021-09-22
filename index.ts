d4a9837fe618c3d826d3dade2691a06ab9f9b509
Jhon Sebastian Bohorquez Guerrero <jhonsbg@gmail.com>
2021-09-21 22:38:07 -0500

    Servidor de webSockets en el que se reciben y se emiten datos

--- index.ts
+++ index.ts
@@ -4,14 +4,14 @@ import router from './routes/router';
 import bodyParser from 'body-parser';
 import cors from 'cors';
 
-const server = new Server();
+const server = Server.instance;
 
 // BodyParser
 server.app.use(bodyParser.urlencoded({extended: true}));
 server.app.use(bodyParser.json());
 
 // CORS
-server.app.use(cors({origin: true, credentials: true}));
+// server.app.use(cors({origin: true, credentials: true}));
 
 // Rutas de servicios
 server.app.use('/', router);
